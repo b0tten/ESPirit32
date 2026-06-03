574799c6794146072a7bf3c8499e028b64a1f67d
b0tten <nikitatodosiev@gmail.com>
2026-06-03 21:49:19 +0200

    FUCK GITHUB

--- ESPirit32-main/examples/Hello_World.c
+++
@@ -1,7 +0,0 @@
-//This is an early look at the programming language (subject of change)
-#include "bios.h"
-
-void game_entry(const bios_api_t* api)
-{
-    api->print("Hello World!");
-}
--- README.md
+++
@@ -1,2 +0,0 @@
-# **ESPirit32: fuck you**
-this project will never be released.
